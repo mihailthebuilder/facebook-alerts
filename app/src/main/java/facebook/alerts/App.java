869d1da5253dbccb17a9c8d5393b0027d26bfba9
Mihail Marian <mihailmarian12@gmail.com>
2022-06-23 07:47:37 +0100

    creating browser instance

--- app/src/main/java/facebook/alerts/App.java
+++ app/src/main/java/facebook/alerts/App.java
@@ -3,12 +3,23 @@
  */
 package facebook.alerts;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(new App().getGreeting());
+
+        System.setProperty("webdriver.chrome.driver",
+                System.getProperty("user.dir") + "/src/main/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("youtube.com");
+        driver.close();
     }
 }
