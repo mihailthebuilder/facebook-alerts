4f5f366cef461c133a7bd3f897b11dbdaaa7fcc4
Mihail Marian <mihailmarian12@gmail.com>
2022-06-23 20:34:28 +0100

    removed templated stuff

--- app/src/main/java/facebook/alerts/App.java
+++ app/src/main/java/facebook/alerts/App.java
@@ -7,19 +7,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(new App().getGreeting());
 
         System.setProperty("webdriver.chrome.driver",
                 System.getProperty("user.dir") + "/src/main/resources/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://youtube.com");
+        driver.get("https://facebook.com");
         Thread.sleep(10000);
         driver.close();
     }
