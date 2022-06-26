d4e5d65ead3618f0f957b45de3f52ad76f9d7f06
Mihail Marian <mihailmarian12@gmail.com>
2022-06-26 20:49:47 +0100

    created separate object

---
+++ app/src/main/java/facebookalerts/Scraper/Scraper.java
@@ -0,0 +1,9 @@
+package facebookalerts.Scraper;
+
+import java.time.LocalDateTime;
+
+public class Scraper {
+    public boolean getPosts(String facebookGroupUrl, String[] keywords, LocalDateTime dateTimeOfLastScrapedPost) {
+        return true;
+    }
+}
