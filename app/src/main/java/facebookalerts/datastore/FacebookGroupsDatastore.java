a9a23bfd4e69d45222283ed14c1f6cfc0414fc81
Mihail Marian <mihailmarian12@gmail.com>
2022-06-28 21:12:02 +0100

    relative directory of db

--- app/src/main/java/facebookalerts/datastore/FacebookGroupsDatastore.java
+++ app/src/main/java/facebookalerts/datastore/FacebookGroupsDatastore.java
@@ -3,10 +3,14 @@ package facebookalerts.datastore;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class FacebookGroupsDatastore {
-    private String dbLocation = "/home/mmarian/dev/facebook-alerts/app/src/main/resources/db/FacebookGroups.json";
+
+    String projectPath = System.getProperty("user.dir");
+
+    private String dbLocation = projectPath + "/src/main/resources/db/FacebookGroups.json";
 
     public FacebookGroupRecord[] getAllFacebookGroups()
             throws FileNotFoundException, IOException, ClassNotFoundException {
