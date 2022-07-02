a2d4100e8094df68260a302dc918f336cdb43a50
Mihail Marian <mihailmarian12@gmail.com>
2022-07-02 05:52:09 +0100

    test for scraper

---
+++ app/src/test/java/facebookalerts/scraper/ScraperTest.java
@@ -0,0 +1,32 @@
+package facebookalerts.scraper;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import facebookalerts.records.FacebookGroupRecord;
+import facebookalerts.records.KeywordRecord;
+import facebookalerts.records.UserNotificationRecord;
+
+public class ScraperTest {
+    @Test
+    void getUserNotifications() throws InterruptedException {
+
+        Scraper scraper = new Scraper();
+
+        Instant yesterday = Instant.now().minus(1, ChronoUnit.DAYS);
+
+        KeywordRecord keyword = new KeywordRecord("free", new String[] { "test@test.com" });
+
+        String projectPath = System.getProperty("user.dir");
+
+        String htmlLocation = projectPath + "/src/test/resources/FacebookGroupPage.html";
+
+        FacebookGroupRecord group = new FacebookGroupRecord("https://www.nba.com", "yesterday",
+                new KeywordRecord[] { keyword });
+
+        List<UserNotificationRecord> notificationList = scraper.getUserNotifications(group, yesterday);
+    }
+}
