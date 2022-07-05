03566414ce861ef96d6dff20417b8690c9fea075
Mihail Marian <mihailmarian12@gmail.com>
2022-07-05 14:35:19 +0100

    notifications

--- app/src/main/java/facebookalerts/notifier/Notifier.java
+++ app/src/main/java/facebookalerts/notifier/Notifier.java
@@ -3,7 +3,20 @@ package facebookalerts.notifier;
 import java.util.List;
 import java.util.Map;
 
+import facebookalerts.records.KeywordRecord;
+
 public class Notifier {
-    public void sendNotification(Map<String, List<String>> userNotification) {
+
+    private Map<String, List<String>> notifications;
+
+    protected Map<String, List<String>> getNotifications() {
+        return this.notifications;
+    }
+
+    protected void setNotifications(Map<String, List<String>> notifications) {
+        this.notifications = notifications;
+    }
+
+    public void addNotificationsForGroup(List<String> posts, List<KeywordRecord> keywords) {
     }
 }
